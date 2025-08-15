7ec734286d9e25cec1041392121355fc1a4bfa97
Harivinay003 <harivinay003@gmail.com>
2025-08-15 11:50:12 +0530

    Initial commit

---
+++ Battery.py
@@ -0,0 +1,25 @@
+# pip install psutil
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent <= 30 and plugged!=True:
+ 
+    # pip install py-notifier
+    # pip install win10toast
+    from pynotifier import Notification
+
+    Notification(
+        title="Battery Low",
+        description=str(percent) + "% Battery remain!!",
+        duration=5,  # Duration in seconds
+        
+    ).send()
+else:
+    Notification(
+        title="Battery Status",
+        description="Battery is fine",
+        duration=5,
+    ).send()
